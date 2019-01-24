7ebc5aa0c6c52aaa7f0dfb1571705964eb72174e
jiangzhangy <649768017@qq.com>
2019-01-24 12:00:14 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,8 +5,20 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title','weibo')-美尔曼博客</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-    @yield('content')
+    <nav class="navbar navbar-expand-lg navbar-dark bg-warning">
+        <div class="container">
+            <a href="/" class="navbar-brand">美尔曼-微博</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item"><a href="/help" class="nav-link text-white">帮助</a></li>
+                <li class="nav-item"><a href="#" class="nav-link text-white">登录</a></li>
+            </ul>
+        </div>
+    </nav>
+   <div class="container">
+       @yield('content')
+   </div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>您好！欢迎来到美尔曼微博平台</h1>
+        <p class="lead">
+            这里是我们 <a href="http://www.meierman.com" target="_blank">美尔曼医疗美容</a> 的官网！
+        </p>
+        <p>
+            结交爱美伙伴，从这里开始！
+        </p>
+        <p>
+            <a href="#" class="btn btn-lg btn-success" role="button">现在注册</a>
+        </p>
+    </div>
 @stop

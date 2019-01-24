b793abd881c67b29fadb20de75dd7fffef0ce987
jiangzhangy <649768017@qq.com>
2019-01-24 14:56:00 +0800

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -9,7 +9,7 @@
             结交爱美伙伴，从这里开始！
         </p>
         <p>
-            <a href="#" class="btn btn-lg btn-success" role="button">现在注册</a>
+            <a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a>
         </p>
     </div>
 @stop
